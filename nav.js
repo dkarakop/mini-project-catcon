7b6879cbec4cfb135ce47ddf7a5d9485e577b7e8
Dimitra Karakopoulou <126284048+dkarakop@users.noreply.github.com>
2024-09-02 15:07:53 +0300

    Add documentation to nav.js

--- nav.js
+++ nav.js
@@ -1,16 +1,14 @@
+// Wait for the DOM to fully load before executing the script
 document.addEventListener("DOMContentLoaded", function () {
 	const burger = document.getElementById("burger");
 	const dropdownContent = document.querySelector(".burger-dropdown-content");
 
+	// Add a click event listener to the burger menu icon
 	burger.addEventListener("click", function () {
-		// Toggle the active class on the burger menu
 		burger.classList.toggle("active");
-
-		// Toggle the dropdown visibility
 		dropdownContent.classList.toggle("active");
 	});
-
-	// Optionally close the menu when a link is clicked
+	// Add a click event listener to the dropdown content
 	dropdownContent.addEventListener("click", function () {
 		burger.classList.remove("active");
 		dropdownContent.classList.remove("active");
